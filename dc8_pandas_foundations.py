218cc7234b770f1c086cb814d09bba60ebd216d5
elixias <elixias@gmail.com>
2019-09-17 17:13:36 +0800

    dc8

--- dc8_pandas_foundations.py
+++ dc8_pandas_foundations.py
@@ -23,8 +23,8 @@ print(pd.DataFrame(data))
 #data.columns = ['a','b']
 
 """no headers, set column names, set na values, parse columns as dates"""
-#data = pd.read_csv(filepath, header=None, name=[...,...], na_values={'column':['-1']},
-#parse_dates=[[0,1,2]]) 
+#data = pd.read_csv(filepath, header=None, names=[...,...], na_values={'column':['-1']},
+#parse_dates=[[0,1,2]], index_col='')
 
 """set index"""
 #data.index=data['yearmthday']
@@ -72,3 +72,56 @@ plt.ylabel('')
 #data.column.count() #applied to series
 #data.count() #applied to df, returns a series of the counts
 #count(),mean(),median(),std(),quantile(0.X) or quantile([0.X,0.X]), min(), max()
+#data.column.unique()
+
+#indices = data['species'] == 'setosa'
+#data[indices,:]
+
+"""computing errors"""
+error_setosa = 100 * np.abs(setosa.describe()-data.describe())
+error_setosa = error_setosa/setosa.describe()
+
+#using datetime as indices are very common techniques
+#you can provide the date and a subset of those rows will be returned
+#data.loc['2015-2-5'] #partial datetime string selection
+#data.loc['2015-2':'2016-2'] #partial selection by month range
+#pd.to_datetime([...,...])
+#data.reindex([...,...],method='ffill') #forward fill using preceding entries, or use bfill
+
+#manual way without date index
+#res = df1['Date'].apply(lambda x:x[0:12]=='2010-Aug-01')
+#df1[df1['Date'].apply(lambda x:x[0:8]=='20100801')]
+#df3.loc['2010-Aug-01']
+
+#runtime converting datetime 
+#pd.to_datetime(series, format='%Y-%m-%d %H:%M')
+
+"""reindexing"""
+#ts3 = ts2.reindex(ts1.index)
+
+"""resampling, downsampling (daily to weekly), upsampling (daily to hourly) """
+#daily_mean = sales.resample('D').mean()/.sum()/.max()/.count()/.var()/.std()/.ffill() #D stands for daily
+#min/T, H, D, B, W, M, Q, A
+
+"""rolling mean"""
+#df['Temperature']['2010-Aug-01':'2010-Aug-15'].rolling(window=24).mean()
+#if you use resampling and reindex to do it, looks 'digital'
+#test = unsmoothed.resample('D').mean()
+#test = test.reindex(unsmoothed.index, method='ffill')
+
+#another way is to use interpolate
+df.resample('A').first().interpolate('linear')
+
+"""use str/contains to do string based search/etc"""
+#df.str.upper()/contains().sum()/strip()
+#df.dt.hour #this extracts the hour from the time
+#df.dt.tz_localize('US/Central') #localize time ones is not the same as tz_convert()
+
+#df.columns = df.columns.str.strip()
+
+"""plotting time series data"""
+#correcting data
+#pd.to_numeric(errors='coerce')/to_date/to_str
+
+"""pearson's correlation"""
+#df.corr() #on a df with 2 columns
